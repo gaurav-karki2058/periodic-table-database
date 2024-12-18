7987ab957db6da81aaf5fb4fef756169db6d22ce
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-18 05:33:37 +0000

    refactor: modified invalid message showing method

--- element.sh
+++ element.sh
@@ -1,18 +1,18 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 INVALID_ELEMENT(){
-echo "Please provide an element as an argument."
+echo -e "\n$1"
 }
 
 if [[ -z $1 ]]
 then
-  INVALID_ELEMENT
+  INVALID_ELEMENT "Please provide an element as an argument."
 
   else
    
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    ARGUMENT_QUERY_RESULT=$($PSQL"SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$1")
+    ARGUMENT_QUERY_RESULT=$($PSQL "SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$1")
     if [[ -z $ARGUMENT_QUERY_RESULT ]]
     then
       echo "Not a valid atomic number"
