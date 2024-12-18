b052b67b26f467649a88a5c3bd7fe639be05a35e
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-18 06:25:36 +0000

    feat: fetched element details

--- element.sh
+++ element.sh
@@ -1,7 +1,7 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 INVALID_ELEMENT(){
-echo -e "\n$1"
+echo -e "$1"
 }
 
 if [[ -z $1 ]]
@@ -37,8 +37,15 @@ then
     INVALID_ELEMENT "Invalid input pattern"
   fi
 
-  QUERY_BY_ATOMIC_NUMBER=$($PSQL"SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$ATOMIC_NUMBER")
-  
-  IFS='|' read -r ATOMIC_NUM ELEMENT_TYPE ATOMIC_WEIGHT MELTING_POINT BOILING_POINT <<< "$QUERY_BY_ATOMIC_NUMBER"
-  
+  if [[ $ATOMIC_NUMBER -ne 0 ]]
+  then
+  PROPERTIES_BY_ATOMIC_NUMBER=$($PSQL"SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$ATOMIC_NUMBER")
+  ELEMENT_BY_ATOMIC_NUMBER=$($PSQL "SELECT * FROM ELEMENTS WHERE ATOMIC_NUMBER=$ATOMIC_NUMBER")
+  IFS='|' read -r ATOMIC_NUM ELEMENT_TYPE ATOMIC_WEIGHT MELTING_POINT BOILING_POINT <<< "$PROPERTIES_BY_ATOMIC_NUMBER"
+  IFS='|' read -r ATMOIC_NUM SYMBOL NAME <<< "$ELEMENT_BY_ATOMIC_NUMBER"
+
+  echo "The element with atomic number $ATOMIC_NUM is $NAME ($SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_WEIGHT amu. $ELEMENT_TYPE has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+
+  fi
+
 fi
