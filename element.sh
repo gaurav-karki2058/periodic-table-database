5573f38dfe0e853f7d61e65669ad59e68ff39916
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-18 05:31:09 +0000

    feat: added data fetching by atomic number

--- element.sh
+++ element.sh
@@ -1,1 +1,25 @@
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+INVALID_ELEMENT(){
+echo "Please provide an element as an argument."
+}
+
+if [[ -z $1 ]]
+then
+  INVALID_ELEMENT
+
+  else
+   
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ARGUMENT_QUERY_RESULT=$($PSQL"SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$1")
+    if [[ -z $ARGUMENT_QUERY_RESULT ]]
+    then
+      echo "Not a valid atomic number"
+    else
+      echo $ARGUMENT_QUERY_RESULT
+    fi
+  
+
+  fi
+fi
