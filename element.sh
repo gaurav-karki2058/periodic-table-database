ec74677e75de6507755c2cdf9d98e366c0c967c9
Gaurav Karki <gaurav.karki2058@gmail.com>
2024-12-18 05:47:59 +0000

    feat: added regex pattern for matching element names

--- element.sh
+++ element.sh
@@ -9,19 +9,33 @@ then
   INVALID_ELEMENT "Please provide an element as an argument."
 
   else
-   
+  ATOMIC_NUMBER=0
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    ARGUMENT_QUERY_RESULT=$($PSQL "SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$1")
-    if [[ -z $ARGUMENT_QUERY_RESULT ]]
+    QUERY_BY_ATOMIC_NUMBER=$($PSQL "SELECT * FROM PROPERTIES WHERE ATOMIC_NUMBER=$1")
+    if [[ -z $QUERY_BY_ATOMIC_NUMBER ]]
     then
-      echo "Not a valid atomic number"
+      INVALID_ELEMENT "Not a valid atomic number"
     else
-      echo $ARGUMENT_QUERY_RESULT
+      echo $QUERY_BY_ATOMIC_NUMBER
     fi
   elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
   then
-  echo "yay"
-
+    ATOMIC_NUMBER_BY_SYMBOL=$($PSQL "SELECT ATOMIC_NUMBER FROM PROPERTIES WHERE SYMBOL=$1")
+    if [[ -z $ATOMIC_NUMBER_BY_SYMBOL ]]
+    then
+      INVALID_ELEMENT "No such atomic symbol found"
+    else
+      ATOMIC_NUMBER=$ATOMIC_NUMBER_BY_SYMBOL;
+    fi 
+  elif [[ $1 =~ ^[A-Z][a-z]*$ ]]
+  then
+    ATOMIC_NUMBER_BY_NAME=$($PSQL "SELECT ATOMIC_NUMBER FROM PROPERTIES WHERE NAME=$1")
+    if [[ -z $ATOMIC_NUMBER_BY_NAME ]]
+    then
+      INVALID_ELEMENT "No such atomic symbol found"
+    else
+      ATOMIC_NUMBER=$ATOMIC_NUMBER_BY_NAME;
+    fi 
   fi
 fi
